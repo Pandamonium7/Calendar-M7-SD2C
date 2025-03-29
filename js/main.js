9464e7e303813d4e64c525e20f3d7c8251587e3d
Arturo <38075@ma-web.nl>
2025-03-29 16:42:51 +0100

    initial design

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*let body = document.getElementsByTagName('body');
+
+
+
+
+
+
+console.log(body);
+
+let d = new Date();
+
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+let crntYear = d.getFullYear();
+let crntDay = d.getDay();
+let crntMonth = d.getMonth(); 
+
+console.log(months[crntMonth]);*/
+
